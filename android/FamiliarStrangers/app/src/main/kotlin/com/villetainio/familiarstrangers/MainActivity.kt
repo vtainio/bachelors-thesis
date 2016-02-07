f3cbc98acccf6cfceba11b1acaadc841c01195ee
Ville Tainio <tainio.ville@gmail.com>
2016-02-07 17:37:17 +0200

    Add initial files for android

---
+++ android/FamiliarStrangers/app/src/main/kotlin/com/villetainio/familiarstrangers/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.villetainio.familiarstrangers
+
+import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+
+class MainActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
