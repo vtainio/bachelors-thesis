65e2ee2db7a277ec3b2887d27ca63f2ff1e48ab5
Ville Tainio <tainio.ville@gmail.com>
2016-02-09 17:31:14 +0200

    Try out a simple Anko view

--- android/FamiliarStrangers/app/src/main/kotlin/com/villetainio/familiarstrangers/MainActivity.kt
+++ android/FamiliarStrangers/app/src/main/kotlin/com/villetainio/familiarstrangers/MainActivity.kt
@@ -2,11 +2,15 @@ package com.villetainio.familiarstrangers
 
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
+import android.support.v7.app.AppCompatActivity
+import com.villetainio.familiarstrangers.ui.MainActivityUI
 
-class MainActivity : FragmentActivity() {
+import org.jetbrains.anko.*
+
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MainActivityUI().setContentView(this)
     }
 }
