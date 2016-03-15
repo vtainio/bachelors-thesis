7fba2ac4336e680a9839181637d32e805c43f51c
Ville Tainio <tainio.ville@gmail.com>
2016-03-15 21:32:48 +0200

    Detect Enter and Exit events for beacons

--- android/FamiliarStrangers/app/src/main/kotlin/com/villetainio/familiarstrangers/FamiliarStrangers.kt
+++ android/FamiliarStrangers/app/src/main/kotlin/com/villetainio/familiarstrangers/FamiliarStrangers.kt
@@ -1,12 +1,44 @@
 package com.villetainio.familiarstrangers
 
 import android.app.Application
+import com.estimote.sdk.BeaconManager
+import com.estimote.sdk.Region
 import com.firebase.client.Firebase
+import com.estimote.sdk.Beacon
+import android.util.Log
+import com.villetainio.familiarstrangers.util.Constants
+import java.util.*
 
 class FamiliarStrangers : Application() {
+    val TAG = "BeaconService"
 
     override fun onCreate() {
         super.onCreate()
         Firebase.setAndroidContext(this)
+        val firebase = Firebase(Constants.SERVER_URL)
+
+        val beaconManager = BeaconManager(applicationContext)
+
+        // Connect to every beacon
+        beaconManager.connect { beaconManager.startMonitoring(Region("all beacons", null, null, null)) }
+
+        // Monitor for Enter and Exit events.
+        beaconManager.setMonitoringListener(object: BeaconManager.MonitoringListener {
+            override fun onEnteredRegion(region: Region, list: List<Beacon>) {
+                Log.d(TAG, "Beacon found");
+
+                // Store encounter to Firebase
+                storeEncounter(list.first().macAddress.toHexString(), firebase)
+            }
+
+            override fun onExitedRegion(region: Region) {
+                Log.d(TAG, "Beacon exited")
+                // No need to do anything since the interaction is already stored
+            }
+        })
+    }
+
+    fun storeEncounter(macAddress: String, firebase: Firebase) {
+        //TODO store encounter to Firebase based on the beacon mac address.
     }
 }
